a7bab9a6a824fffaf847f63c0f7091531399a015
Sunil Mannava <mannavasunil@gmail.com>
2025-02-15 14:28:27 +0530

    first workflow test

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
 public static void main(String[] args){
 System.out.println("===a int value===="+(int)'a');
 System.out.println("===a int value===="+(char)48);
+System.out.println("push workflow test");
 }
 }
